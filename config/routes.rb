9688adcc88d5179c7fd8d0f2d5cb5aeb0dc85d4d
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-15 22:08:52 +0700

    follower following, view profile followers, following

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "dashboard", to: "accounts#index"
   get "profile/:username", to: "accounts#profile", as: :profile
   get "post/like/:post_id", to: "likes#save_like", as: :like_post
+  post "follow/account", to: "accounts#follow_account", as: :follow_account
   resources :posts, only: [:show, :new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_085425) do
+ActiveRecord::Schema.define(version: 2020_04_15_153144) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_04_15_085425) do
     t.string "website"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "followers", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.index ["follower_id"], name: "index_followers_on_follower_id"
+    t.index ["following_id"], name: "index_followers_on_following_id"
   end
 
   create_table "likes", force: :cascade do |t|
